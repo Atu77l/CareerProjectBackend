c983eed758762aef947f98690f0f44ae753b10c2
Atu77l <kesharwaniatul9935@gmail.com>
2023-05-01 00:38:48 +0530

    addition

---
+++ index.js
@@ -0,0 +1,79 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const bodyparser=require('body-parser')
+const connectDB=require('./connectDB')
+const User=require('./model')
+require('dotenv');
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'e-com';
+const cors=require('cors');
+const Contact=require('./contactmodel')
+
+
+const app=express();
+app.use(express.json());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(cors());
+
+mongoose.set("strictQuery", false);
+connectDB();
+
+app.get('/get',async(req,res)=>{
+    const data=await User.find();
+    res.send({"detail":data})
+})
+app.post('/save',async(req,res) =>{
+    console.log(req.body);
+    const data=req.body;
+    const user=new User(data);
+    const result=await user.save();
+    res.send({"result":result});
+   
+})
+
+app.post('/contact',async(req,res) =>{
+    console.log(req.body);
+    const data=req.body;
+    const contact=new Contact(data);
+    const result=await contact.save();
+    res.send({"result":result});
+})
+app.get('/get/contact',async(req,res)=>{
+    const data=await Contact.find();
+    res.send({"detail":data})
+})
+app.delete('/delete/:id',async(req,res)=>{
+    const data=await User.deleteOne({ _id: req.params.id });
+    res.send(JSON.stringify(data));
+})
+
+app.patch('/update/:id', async(req,res)=>{
+    const data=User.findOne({_id:req.params.id});
+    const response=await User.updateOne({ _id: req.params.id },
+        { $set: req.body });
+    res.send(response);
+})
+// app.post('/login',async(req,res)=>{
+//     console.log("hello");
+//     if (req.body.password && req.body.email) {
+//         let user = await User.findOne(req.body).select("-password");
+//         if (user) {
+//             Jwt.sign({user}, jwtKey, {expiresIn:"2h"},(err,token)=>{
+//                 if(err){
+//                     res.send("Something went wrong")  
+//                 }
+//                 res.send({user,auth:token})
+//             })
+//         } else {
+//             res.send({ result: "No User found" })
+//         }
+//     } else {
+//         res.send({ result: "No User found" })
+//     }    
+// })
+app.listen(4000,()=>{
+    console.log("connected to port");
+})
+
+
